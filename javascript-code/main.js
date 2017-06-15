fe2094cbbb4438ce792a2ac638f6134cf2b46abe
Loïc Mangeonjean <loic@codingame.com>
2017-06-15 11:52:29 +0200

    test

--- javascript-code/main.js
+++ javascript-code/main.js
@@ -46,7 +46,7 @@ fs.readFile('exercise.js', 'utf8', function(err, data) {
 		var charCount = data.length;
 
 		if (charCount > maxCharCount) {
-			console.out(`You must remove ${charCount - maxCharCount} more character`);
+			console.log(`You must remove ${charCount - maxCharCount} more character`);
 			success = false;
 		}
 		console.log(`TECHIO> success ${success}`)
